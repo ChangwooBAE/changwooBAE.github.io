da9cc09b9dd563f22ec80f8c9f23c14d7fc55faa
ChangwooBAE <101053763+ChangwooBAE@users.noreply.github.com>
2026-05-25 16:30:40 +0000

    Deploying to gh-pages from @ ChangwooBAE/changwooBAE.github.io@e5e7b499904ff0ad126696243f76fd6ffd5f2270 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ChangwooBAE/cbae_test.github.io",
+    "data-repo": "ChangwooBAE/changwooBAE.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
